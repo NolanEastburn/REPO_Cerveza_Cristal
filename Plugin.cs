0ad23fba836158439ee8f44c65afe0ccc1a00feb
NolanEastburn <nolieman1@gmail.com>
2025-05-28 13:58:25 -0500

    -Created a test mesh.

--- Plugin.cs
+++ Plugin.cs
@@ -2,6 +2,7 @@
 using BepInEx;
 using BepInEx.Logging;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Cerveza_Cristal;
 
@@ -11,8 +12,8 @@ public class Plugin : BaseUnityPlugin
     internal static new ManualLogSource Logger;
 
     private Boolean spawned = false;
-    private const float CRASH_TIME = 5;
-        
+    private const float CRASH_TIME = 1;
+
     private void Awake()
     {
         // Plugin startup logic
@@ -24,17 +25,34 @@ public class Plugin : BaseUnityPlugin
     private void Update()
     {
 
-        if(Time.time >= CRASH_TIME && !spawned)
+
+        if (Time.time >= CRASH_TIME && !spawned)
         {
+
+            ResourcesAPI resources = new ResourcesAPI();
+
+            GameObject testValuable = new GameObject("testValuable");
+            MeshFilter mf = testValuable.AddComponent<MeshFilter>();
+            MeshRenderer mr = testValuable.AddComponent<MeshRenderer>();
+            mr.material.color = new Color(1.0f, 0f, 0f);
+            //mf.sharedMesh = resources.Load("../Resources/Meshes/TestCone.fbx", typeof(Mesh)) as Mesh;
+
+            mf.sharedMesh = Resources.Load<Mesh>("Meshes/TestCone.fbx");
+
+            if (mf.sharedMesh == null)
+            {
+                Logger.LogError("Could not load the cone mesh!");
+            }
             // Find truck.
 
             GameObject truck = null;
 
-            foreach(GameObject g in UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects())
+
+            foreach (GameObject g in UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects())
             {
                 TruckLandscapeScroller obj = g.GetComponentInChildren<TruckLandscapeScroller>();
 
-                if(obj != null)
+                if (obj != null)
                 {
                     truck = obj.gameObject;
                 }
@@ -44,6 +62,9 @@ public class Plugin : BaseUnityPlugin
             GameObject thing = UnityEngine.Object.Instantiate(AssetManager.instance.surplusValuableBig, truck.transform.position + new Vector3(-35, 10, 0), Quaternion.identity);
             thing.transform.localScale = new Vector3(5, 10, 5);
             Logger.LogInfo("Spawned a thing!");
+
+            // Add the valuable to the scene.
+            testValuable.SetActive(true);
         }
 
     }
