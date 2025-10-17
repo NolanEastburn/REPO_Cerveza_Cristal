223312fa606726eeac1aa9fada329725f90e3ce8
Nolan Eastburn <nolieman1@gmail.com>
2025-10-16 19:47:42 -0500

    -Made the entries in ModValuables cleaner.

--- src/ModValuables.cs
+++ src/ModValuables.cs
@@ -3,11 +3,20 @@ namespace Cerveza_Cristal;
 public static class ModValuables
 {
     public static readonly ModValuableRegistry.ValuableAddition TEST_VALUABLE =
-     new ModValuableRegistry.ValuableAddition(assetName: "Cone", valuableData: new ModValuableRegistry.Data(name: "Test Valuable"));
+     new ModValuableRegistry.ValuableAddition(assetName: "Cone", valuableData: new ModValuableRegistry.Data(
+        name: "Test Valuable"
+    ));
 
     public static readonly ModValuableRegistry.ValuableAddition BOTTLE =
     new ModValuableRegistry.ValuableAddition(assetName: "CervezaCristal", valuableData: new ModValuableRegistry.Data
-    (name: "Cerveza Cristal Bottle", value: (1000, 3000), mass: 0.25f, ValuableVolume.Type.Small, durability: 5.0f, fragility: 80.0f));
+    (
+        name: "Cerveza Cristal Bottle",
+        value: (1000, 3000),
+        mass: 0.25f,
+        ValuableVolume.Type.Small,
+        durability: 5.0f,
+        fragility: 80.0f
+    ));
 
     public static ModValuableRegistry.ValuableAddition[] ValuableAdditions { get; private set; } = { TEST_VALUABLE, BOTTLE };
 }
