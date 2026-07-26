0836c067519a9c1bca10c9f3aa5a1b8c19f86945
Ubuntu <nooterdooter@arch-noot-laptop.lan>
2026-07-26 01:28:20 +0000

    -Drafted up the initial jingle code with the door detection.

--- src/MonoBehaviours/JingleBehaviour.cs
+++ src/MonoBehaviours/JingleBehaviour.cs
@@ -5,13 +5,30 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 class JingleBehaviour : MonoBehaviour
 {
+    private const float OPEN_ANGLE = 80.0f;
+
+    private float _targetTime { get; set; } = 0.0f;
+
+    private const float TARGET_TIME_DELTA = 0.5f;
+
     private bool _foundDoor { get; set; } = false;
+
+    private Quaternion _startingRotation { get; set; } = Quaternion.identity;
+
     private bool _doorOpened { get; set; }
 
     private GameObject _door { get; set; }
+
+    private AudioSource _jingle { get; set; }
+
+    public void Start()
+    {
+        _jingle = gameObject.GetComponent<AudioSource>();
+    }
 
     public void OnEnable()
     {
@@ -41,17 +58,9 @@ class JingleBehaviour : MonoBehaviour
                 if (doorRegex.IsMatch(hitObject.name))
                 {
                     _door = hitObject;
+                    _startingRotation = _door.transform.rotation;
                     _foundDoor = true;
-                    ModEntry.Instance.Logger.LogInfo("Found the door!!!!");
                 }
-                else
-                {
-                    ModEntry.Instance.Logger.LogInfo($"Did not hit the door. What was hit: {hitObject.name}");
-                }
-            }
-            else
-            {
-                ModEntry.Instance.Logger.LogInfo("Bottle did hit anything on the door search raycast");
             }
 
             yield return new WaitForSeconds(10.0f);
@@ -60,12 +69,30 @@ class JingleBehaviour : MonoBehaviour
         yield break;
     }
 
+    private IEnumerator PlayJingle()
+    {
+        _jingle.Play();
+        ModEntry.Instance.Logger.LogDebug("Playing jingle!");
+        yield return new WaitUntil(() => !_jingle.isPlaying);
+        ModEntry.Instance.Logger.LogDebug("Done playing jingle!");
+        Destroy(this);
+        yield break;
+    }
+
     public void Update()
     {
-        // Only look for the door opening once the door is found.
-        if (_foundDoor)
+        if (Time.time >= _targetTime && !_doorOpened && _foundDoor)
         {
-
+            if (Quaternion.Angle(_startingRotation, _door.transform.rotation) >= OPEN_ANGLE)
+            {
+                // Play the jingle and then cleanup this object via a co-routine.
+                _doorOpened = true;
+                StartCoroutine(PlayJingle());
+            }
+            else
+            {
+                _targetTime = Time.time + TARGET_TIME_DELTA;
+            }
         }
     }
 }
